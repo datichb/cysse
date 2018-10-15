5d755097d2c93432d661e2bc5bd864d75ea1b56e
Ben Datiche <datich_b@MacBook-Pro-de-Benjamin.local>
2018-10-15 11:02:35 +0200

    add Envoy

---
+++ resources/views/Envoy.blade.php
@@ -0,0 +1,39 @@
+@servers(['web' => 'git@15.196.88.187'])
+
+@setup
+    $repository = 'git@gitlab.com:datiche_b/cysse.git';
+    $releases_dir = '/var/www/app/releases';
+    $app_dir = '/var/www/cysse';
+    $release = date('YmdHis');
+    $new_release_dir = $releases_dir .'/'. $release;
+@endsetup
+
+@story('deploy')
+    clone_repository
+    run_composer
+    update_symlinks
+@endstory
+
+@task('clone_repository')
+    echo 'Cloning repository'
+    [ -d {{ $releases_dir }} ] || mkdir {{ $releases_dir }}
+    git clone --depth 1 {{ $repository }} {{ $new_release_dir }}
+@endtask
+
+@task('run_composer')
+    echo "Starting deployment ({{ $release }})"
+    cd {{ $new_release_dir }}
+    composer install --prefer-dist --no-scripts -q -o
+@endtask
+
+@task('update_symlinks')
+    echo "Linking storage directory"
+    rm -rf {{ $new_release_dir }}/storage
+    ln -nfs {{ $app_dir }}/storage {{ $new_release_dir }}/storage
+
+    echo 'Linking .env file'
+    ln -nfs {{ $app_dir }}/.env {{ $new_release_dir }}/.env
+
+    echo 'Linking current release'
+    ln -nfs {{ $new_release_dir }} {{ $app_dir }}/current
+@endtask
